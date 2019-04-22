199b2679843d75a225892aee802ea94e65ceeb2d
Rodrigo Martins <rodrigo.martins1@everis.com>
2019-04-22 13:22:38 -0300

    Start Project

---
+++ TextToSpeech/TextToSpeech/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TextToSpeech
+//
+//  Created by Rodrigo Martins on 22/04/19.
+//  Copyright © 2019 Rodrigo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
